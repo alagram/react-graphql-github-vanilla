3e2fe6938a262ec935e74bdfdf969d5ca5be3e99
Albert Agram <albert.agram@gmail.com>
2018-09-14 13:27:04 +0000

    first GraphQl query with HTTP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { GET_ORGANIZATION } from "./queries";
 
 const axiosGithubGraphQL = axios.create({
   baseURL: "https://api.github.com/graphql",
@@ -13,10 +14,47 @@ const axiosGithubGraphQL = axios.create({
 const TITLE = "React GraphQL Github Client";
 
 class App extends Component {
+  state = {
+    path: "the-road-to-learn-react/the-road-to-learn-react"
+  };
+
+  componentDidMount() {
+    this.onFetchFromGuthub();
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    // fetch data
+
+    event.preventDefault();
+  };
+
+  onFetchFromGuthub = () => {
+    axiosGithubGraphQL
+      .post("", { query: GET_ORGANIZATION })
+      .then(result => console.log(result));
+  };
+
   render() {
+    const { path } = this.state;
     return (
       <div>
         <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com/</label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ width: "300px" }}
+          />
+          <button type="submit">Search</button>
+        </form>
       </div>
     );
   }
